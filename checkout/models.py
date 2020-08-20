3812833aab8e926bf5ab3b4cd61e3b702b7995a3
sree0831 <sreelakshmi.passavula@gmail.com>
2020-08-20 20:27:28 +0000

    added form to checkout

--- checkout/models.py
+++ checkout/models.py
@@ -5,6 +5,7 @@ from products.models import Product
 
 class Order(models.Model):
     full_name = models.CharField(max_length=50, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, blank=False)
     country = models.CharField(max_length=40, blank=False)
     postcode = models.CharField(max_length=20, blank=True)
