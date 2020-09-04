c3655e6c1fc64b09c1f4da4f44dc10e4fbb243d8
sree0831 <sreelakshmi.passavula@gmail.com>
2020-09-04 20:06:13 +0000

    added profile app

--- checkout/models.py
+++ checkout/models.py
@@ -4,11 +4,15 @@ from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 
-from products.models import Product
+from django_countries.fields import CountryField
 
+from products.models import Product
+from profiles.models import UserProfile
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=True, editable=True)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL,
+                                     null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=True, blank=True)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
--- tech_buddy/urls.py
+++ tech_buddy/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -25,5 +27,7 @@ urlpatterns = [
     path('products/', include('products.urls')),
     path('bag/', include('bag.urls')),
     path('checkout/', include('checkout.urls')),
+    path('profile/', include('profiles.urls')),
+  
        
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
