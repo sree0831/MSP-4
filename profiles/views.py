dc4221ed03be0f225e30f5790d9b1af426d626e6
sree0831 <sreelakshmi.passavula@gmail.com>
2020-09-09 19:40:57 +0000

    added profile

--- profiles/views.py
+++ profiles/views.py
@@ -3,10 +3,15 @@ from django.shortcuts import render, get_object_or_404
 from .models import UserProfile
 from .forms import UserProfileForm
 
-
 def profile(request):
     """ Display the user's profile. """
     profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile updated successfully')
 
     form = UserProfileForm(instance=profile)
     orders = profile.orders.all()
@@ -15,6 +20,9 @@ def profile(request):
     context = {
         'form': form,
         'orders': orders,
+        'on_profile_page': True
     }
 
     return render(request, template, context)
+
+
