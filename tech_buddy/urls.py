a8f9a0c2519265821518f221589fd8482b2c113a
sree0831 <sreelakshmi.passavula@gmail.com>
2020-09-21 18:17:44 +0000

    fixed errors

--- tech_buddy/urls.py
+++ tech_buddy/urls.py
@@ -32,7 +32,5 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('profile/', include('profiles.urls')),
     url(r'^news/', get_posts, name='news'),
-  
-  
-
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
