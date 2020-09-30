e3af503560401bd2462025edc4f9221574647c3f
sree0831 <sreelakshmi.passavula@gmail.com>
2020-09-30 15:48:55 +0000

    updated settings

--- tech_buddy/settings.py
+++ tech_buddy/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['tech-buddy.herokuapp.com', 'localhost']
+X_FRAME_OPTIONS = ['ALLOW-FROM http://ami.responsivedesign.is/']
 
 
 # Application definition
